9bf93ddd26ccc67633ebc61872d27828a5acba5b
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2023-09-07 15:24:16 +0100

    feat: setup the redis module with `ioredis` dependency
    - add config validation for redis credentials
    - add redis as dynamic module

---
+++ backend/src/redis/redis.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { RedisService } from './redis.service';
+import { ConfigurableModuleClass } from './redis.module-definition';
+
+@Module({
+  providers: [RedisService],
+})
+export class RedisModule extends ConfigurableModuleClass {}
---
+++ backend/src/redis/redis.service.ts
@@ -0,0 +1,14 @@
+import { Inject, Injectable } from '@nestjs/common';
+
+import { Redis } from 'ioredis';
+import { MODULE_OPTIONS_TOKEN } from './redis.module-definition';
+import { RedisModuleOptions } from './redis.inreface';
+
+@Injectable()
+export class RedisService extends Redis {
+  constructor(
+    @Inject(MODULE_OPTIONS_TOKEN) private redisOptions: RedisModuleOptions,
+  ) {
+    super(redisOptions);
+  }
+}
