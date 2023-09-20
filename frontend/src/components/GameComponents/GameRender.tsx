c8ee67b8542b1c2503473f507fb9ed427103938a
Said Mazouz <smazouz@e3r10p12.1337.ma>
2023-09-20 16:56:08 +0100

    feat: Enable player paddle to move dynamically with mouse

--- frontend/src/components/GameComponents/GameRender.tsx
+++ frontend/src/components/GameComponents/GameRender.tsx
@@ -18,21 +18,21 @@ const GameRender = () => {
     });
     const ball = Matter.Bodies.circle(325, 375, 10, {
       render: {
-        fillStyle: 'white', // Set the ball's fill color to red
+        fillStyle: "white",
       },
     });
 
-    const playerpadle = Matter.Bodies.rectangle(325,15, 150, 20,{
-      isStatic: true,
+    const playerPaddle = Matter.Bodies.rectangle(325, 15, 150, 20, {
+      isStatic: true, // Make the player paddle non-static
       render: {
         fillStyle: "#8D8DDA",
-       },
-       chamfer: {
+      },
+      chamfer: {
         radius: 10,
       },
     });
 
-    const opentpadle = Matter.Bodies.rectangle(325, 735, 150, 20, {
+    const opponentPaddle = Matter.Bodies.rectangle(325, 735, 150, 20, {
       isStatic: true,
       render: {
         fillStyle: "#8D8DDA",
@@ -44,19 +44,31 @@ const GameRender = () => {
     const horizontalLine = Matter.Bodies.rectangle(325, 375, 650, 1, {
       isStatic: true,
       render: {
-        fillStyle: 'white',
+        fillStyle: "white",
       },
     });
-    Matter.World.add(engine.world, [ball, playerpadle, opentpadle , horizontalLine]);
+    Matter.World.add(engine.world, [ball, playerPaddle, opponentPaddle, horizontalLine]);
     const runner = Matter.Runner.create();
     Matter.Runner.run(runner, engine);
     Matter.Render.run(render);
+
+    const canvas = canvasRef.current;
+
+    // Add mousemove event listener to update the player paddle's position
+    canvas.addEventListener("mousemove", (event) => {
+      const mouseX = event.clientX - canvas.getBoundingClientRect().left;
+      Matter.Body.setPosition(playerPaddle, { x: mouseX, y: playerPaddle.position.y });
+    });
+
     return () => {
       Matter.Render.stop(render);
       Matter.Runner.stop(runner);
       Matter.Engine.clear(engine);
+      // Remove the mousemove event listener when unmounting
+      canvas.removeEventListener("mousemove", () => {});
     };
   }, []);
+
   return <canvas ref={canvasRef} className="w-full h-full rounded-3xl" />;
 };
 
