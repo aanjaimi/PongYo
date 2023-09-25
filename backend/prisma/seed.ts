fa57e992d319e2bd44dc90d51114c548b6be5a56
4DeR99 <moulmado@student.1337.ma>
2023-09-25 12:19:30 +0100

    fear: added seeding feature
    added seed.ts and seedData.ts files where i seed some users channels and
    messages in the data base (awbx zaml madarhomch fach gltha lih)

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from '@prisma/client';
+import { users, channels, messages } from './seedData';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  for (const user of users) {
+    if (await prisma.user.findUnique({ where: { login: user.login } })) {
+      continue;
+    }
+    await prisma.user.create({
+      data: user,
+    });
+  }
+
+  for (const channel of channels) {
+    if (await prisma.channel.findFirst({ where: { name: channel.name } })) {
+      continue;
+    }
+    await prisma.channel.create({
+      data: channel,
+    });
+  }
+
+  for (const message of messages) {
+    if (
+      await prisma.message.findFirst({ where: { content: message.content } })
+    ) {
+      continue;
+    }
+    await prisma.message.create({
+      data: message,
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
---
+++ backend/prisma/seedData.ts
@@ -0,0 +1,291 @@
+import { RoomType } from '@prisma/client';
+
+export const users = [
+  {
+    id: '1',
+    displayName: '4der',
+    login: 'moulmado',
+    email: 'x@g.co',
+  },
+  {
+    id: '2',
+    displayName: 'somo',
+    login: 'aanjaimi',
+    email: 'xx@g.co',
+  },
+  {
+    id: '3',
+    displayName: 'said',
+    login: 'smazouz',
+    email: 'xxx@g.co',
+  },
+  {
+    id: '4',
+    displayName: 'awbx',
+    login: 'asabani',
+    email: 'xxxx@g.co',
+  },
+  {
+    id: '5',
+    displayName: 'ra1nMak3r',
+    login: 'omoussao',
+    email: 'xxxxx@g.co',
+  },
+];
+
+export const channels = [
+  {
+    id: '1',
+    type: RoomType.PUBLIC,
+    name: 'general',
+    isDM: false,
+    members: {
+      connect: [
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+        { id: '4' },
+        { id: '5' },
+      ],
+    },
+  },
+  {
+    id: '2',
+    type: RoomType.PUBLIC,
+    name: 'random',
+    isDM: false,
+    members: {
+      connect: [
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+        { id: '4' },
+        { id: '5' },
+      ],
+    },
+  },
+  {
+    id: '3',
+    type: RoomType.PUBLIC,
+    name: 'forStudents',
+    isDM: false,
+    members: {
+      connect: [
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+        { id: '4' },
+        { id: '5' },
+      ],
+    },
+  },
+  {
+    id: '4',
+    type: RoomType.PUBLIC,
+    name: 'forDevs',
+    isDM: false,
+    members: {
+      connect: [
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+        { id: '4' },
+        { id: '5' },
+      ],
+    },
+  },
+  {
+    id: '5',
+    type: RoomType.PUBLIC,
+    name: 'forStaff',
+    isDM: false,
+    members: {
+      connect: [
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+        { id: '4' },
+        { id: '5' },
+      ],
+    },
+  },
+  {
+    id: '6',
+    type: RoomType.PUBLIC,
+    name: 'food',
+    isDM: false,
+    members: {
+      connect: [
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+        { id: '4' },
+        { id: '5' },
+      ],
+    },
+  },
+];
+
+export const messages = [
+  //channel 1
+  {
+    content: 'message 1',
+    user: {
+      connect: {
+        id: '1',
+      },
+    },
+    channel: {
+      connect: {
+        id: '1',
+      },
+    },
+  },
+  {
+    content: 'message 2',
+    user: {
+      connect: {
+        id: '2',
+      },
+    },
+    channel: {
+      connect: {
+        id: '1',
+      },
+    },
+  },
+  {
+    content: 'message 3',
+    user: {
+      connect: {
+        id: '3',
+      },
+    },
+    channel: {
+      connect: {
+        id: '1',
+      },
+    },
+  },
+  {
+    content: 'message 4',
+    user: {
+      connect: {
+        id: '1',
+      },
+    },
+    channel: {
+      connect: {
+        id: '1',
+      },
+    },
+  },
+  {
+    content: 'message 5',
+    user: {
+      connect: {
+        id: '3',
+      },
+    },
+    channel: {
+      connect: {
+        id: '1',
+      },
+    },
+  },
+  {
+    content: 'message 6',
+    user: {
+      connect: {
+        id: '1',
+      },
+    },
+    channel: {
+      connect: {
+        id: '1',
+      },
+    },
+  },
+  // channel 2
+  {
+    content: 'message 1`',
+    user: {
+      connect: {
+        id: '1',
+      },
+    },
+    channel: {
+      connect: {
+        id: '2',
+      },
+    },
+  },
+  {
+    content: 'message 2`',
+    user: {
+      connect: {
+        id: '1',
+      },
+    },
+    channel: {
+      connect: {
+        id: '2',
+      },
+    },
+  },
+  {
+    content: 'message 3`',
+    user: {
+      connect: {
+        id: '3',
+      },
+    },
+    channel: {
+      connect: {
+        id: '2',
+      },
+    },
+  },
+  {
+    content: 'message 4`',
+    user: {
+      connect: {
+        id: '1',
+      },
+    },
+    channel: {
+      connect: {
+        id: '2',
+      },
+    },
+  },
+  // channel 3\
+  {
+    content:
+      'lorem hamid lazy to search for it so i will just write some random shit and duplicate it xD. lorem hamid lazy to search for it so i will just write some random shit and duplicate it xD.',
+    user: {
+      connect: {
+        id: '1',
+      },
+    },
+    channel: {
+      connect: {
+        id: '3',
+      },
+    },
+  },
+  {
+    content:
+      'lorem hamid lazy to search for it so i will just write some random shit and duplicate it xD. lorem hamid lazy to search for it so i will just write some random shit and duplicate it xD. ',
+    user: {
+      connect: {
+        id: '1',
+      },
+    },
+    channel: {
+      connect: {
+        id: '3',
+      },
+    },
+  },
+];
