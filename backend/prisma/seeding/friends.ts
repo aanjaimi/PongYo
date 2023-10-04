61a29de5e938626d76df0732affacf7658b92f55
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2023-10-04 17:19:12 +0100

    feat: implement the friends seeding
    - seed some users for testing
    - send friend request
    - accept frined request

---
+++ backend/prisma/seeding/friends.ts
@@ -0,0 +1,76 @@
+import { User } from '@prisma/client';
+import prisma from '../utils/db';
+
+type _User = Pick<User, 'displayname' | 'login' | 'email'>;
+
+export const getAvailableUsers = (users: _User[]) =>
+  prisma.$transaction(
+    users.map(({ displayname, email, login }) =>
+      prisma.user.upsert({
+        where: {
+          login,
+        },
+        create: {
+          displayname,
+          email,
+          login,
+          log: {
+            create: {
+              defeats: 0,
+              points: 0,
+              rank: 'UNRANKED',
+              vectories: 0,
+            },
+          },
+        },
+        update: {},
+      }),
+    ),
+  );
+
+export const sendFriendRequest = async (user: User, friend: User) => {
+  const [{ id: userId }, { id: friendId }] = [user, friend];
+  const friendShip = await prisma.friend.findFirst({
+    where: {
+      OR: [
+        { AND: [{ userId }, { friendId }] },
+        { AND: [{ friendId: userId }, { userId: friendId }] },
+      ],
+    },
+  });
+  console.log(
+    `The user ${user.login} sent a friend request to user ${friend.login}`,
+  );
+  if (!friendShip) {
+    return await prisma.friend.create({
+      data: {
+        userId,
+        friendId,
+        state: 'PENDING',
+      },
+    });
+  }
+
+  return friendShip;
+};
+
+export const acceptFriendRequest = async (user: User, friend: User) => {
+  const friendShip = await prisma.friend.findFirst({
+    where: {
+      userId: friend.id,
+      friendId: user.id,
+    },
+  });
+  if (!friendShip) return;
+
+  console.log(
+    `The user ${user.login} accepts friend request of user ${friend.login}`,
+  );
+
+  return await prisma.friend.update({
+    where: { id: friendShip.id },
+    data: {
+      state: 'ACCEPTED',
+    },
+  });
+};
