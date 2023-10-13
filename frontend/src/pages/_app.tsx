aba959737798b3d5c1e228f46bd7a713c9550f2a
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2023-10-13 01:00:57 +0100

    feat: wrap the entire app with `SocketProvider`

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SocketProvider } from "@/contexts/socket-context";
 
 const queryClient = new QueryClient();
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <SocketProvider>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </SocketProvider>
   );
 };
 
