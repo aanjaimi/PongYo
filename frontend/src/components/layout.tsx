98a2343b8d257515e6245f0afb3f437c54830284
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2023-10-22 01:46:52 +0100

    chore: refactor the frontend

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { Sidebar } from "./sidebar";
 import NavBar from "./navbar";
+import { useStateContext } from "@/contexts/state-context";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 export default function Layout({ children }: LayoutProps) {
+  const {
+    state: { authenicated },
+  } = useStateContext();
   return (
     <>
-      <div className="flex h-screen w-screen flex-col">
-        <NavBar />
-        <div className="flex h-full w-full">
-          <div className="">
-            <Sidebar />
+      {authenicated === true && (
+        <div className="flex h-screen w-screen flex-col">
+          <NavBar />
+          <div className="flex h-full w-full">
+            <div className="">
+              <Sidebar />
+            </div>
+            <div className="flex-1 overflow-hidden p-4">{children}</div>
           </div>
-          <div className="flex-1 overflow-hidden p-4">{children}</div>
         </div>
-      </div>
+      )}
+      {!authenicated && <>{children}</>}
     </>
   );
 }
