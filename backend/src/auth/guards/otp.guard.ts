1af9fb66f7aa12f45ae861f7382bb6b6f715d796
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2023-10-25 00:34:08 +0100

    chore: remove old totp guard & strategy
    - add new otp strategy extends the jwt strategy

---
+++ backend/src/auth/guards/otp.guard.ts
@@ -0,0 +1,39 @@
+import { RedisService } from '@/redis/redis.service';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { AUTH_COOKIE_NAME } from '../auth.constants';
+
+@Injectable()
+export class OptAuthGuard extends AuthGuard('jwt') {
+  constructor(private redisService: RedisService) {
+    super();
+  }
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    await super.canActivate(context);
+
+    const req: Request = context.switchToHttp().getRequest();
+
+    const accessToken = req.cookies[AUTH_COOKIE_NAME] as string | null;
+
+    const otpNeed = await this.redisService.hget(
+      `token-${accessToken}`,
+      'otp-needed',
+    );
+
+    if (!otpNeed || otpNeed === '0') throw new UnauthorizedException();
+
+    return true;
+  }
+
+  handleRequest(err: unknown, user: unknown): any {
+    if (err || !user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
