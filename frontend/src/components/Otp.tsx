bb46f8f7f930a1da30c9ace65ce788e65a3fff02
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2023-10-29 14:40:57 +0100

    chore: set `auth_status` to `authenticated` after otp passed

--- frontend/src/components/Otp.tsx
+++ frontend/src/components/Otp.tsx
@@ -1,37 +1,37 @@
-import React from "react";
-import Image from "next/image";
-import { useState } from "react";
-import { Card } from "@/components/ui/card";
-import { useMutation } from "@tanstack/react-query";
-import { fetcher } from "@/utils/fetcher";
-import OTPInput from "react-otp-input";
-import { cn } from "@/lib/utils";
-import { Button } from "./ui/button";
-import { useStateContext } from "@/contexts/state-context";
-import { useRouter } from "next/router";
+import React from 'react';
+import Image from 'next/image';
+import { useState } from 'react';
+import { Card } from '@/components/ui/card';
+import { useMutation } from '@tanstack/react-query';
+import { fetcher } from '@/utils/fetcher';
+import OTPInput from 'react-otp-input';
+import { cn } from '@/lib/utils';
+import { Button } from './ui/button';
+import { useStateContext } from '@/contexts/state-context';
+import { useRouter } from 'next/router';
 
 async function verifyOtp(token: string) {
-  return (await fetcher.post<{ valid: boolean }>("/auth/otp", { token })).data;
+  return (await fetcher.post<{ valid: boolean }>('/auth/otp', { token })).data;
 }
 
 const Otp = () => {
   const { state } = useStateContext();
   const router = useRouter();
-  const [otp, setOtp] = useState<string>("");
+  const [otp, setOtp] = useState<string>('');
   const [correct, setCorrect] = useState<boolean>(true);
   const otpMutation = useMutation({
-    mutationKey: ["otp"],
+    mutationKey: ['otp'],
     mutationFn: verifyOtp,
     onSuccess: async ({ valid }) => {
       setCorrect(valid);
       if (valid) {
-        state.auth_status = true;
-        await router.push("/profile/@me");
+        state.auth_status = 'authenticated';
+        await router.push('/profile/@me');
       }
     },
     onError: () => {
       state.auth_status = false;
-      console.log("error");
+      console.log('error');
     },
   });
 
@@ -44,7 +44,7 @@ const Otp = () => {
     <div className="flex h-full w-full items-center justify-center">
       <Card className="flex h-[420px] w-[350px] flex-col items-center justify-center rounded-[63px] border border-black font-['outfit'] text-black sm:h-[385px] sm:w-[541px]">
         <div className="mb-[20px] flex h-[84px] w-[65px] justify-center">
-          <Image src={"/logo.png"} alt="image" width={500} height={500} />
+          <Image src={'/logo.png'} alt="image" width={500} height={500} />
         </div>
         <div className="mb-[10px] text-[30px] font-bold">2FA</div>
         <div className="mb-[20px] ml-[50px] mr-[50px] text-[15px]">
@@ -64,9 +64,9 @@ const Otp = () => {
             inputType="number"
             shouldAutoFocus
             inputStyle={cn(
-              "border w-10 h-10 font-bold text-[20px] text-center [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+              'border w-10 h-10 font-bold text-[20px] text-center [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none',
             )}
-            containerStyle={cn("grow flex items-center justify-around grow")}
+            containerStyle={cn('grow flex items-center justify-around grow')}
             skipDefaultStyles={true}
           />
         </>
