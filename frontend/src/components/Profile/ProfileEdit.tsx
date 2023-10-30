1dffb82ad9b1766ed71dd5b0bf415c41b1b07464
Ayoub Anjaimi <aanjaimi@student.1337.ma>
2023-10-30 15:47:32 +0100

    fix: Edit profile when your are the owner

--- frontend/src/components/Profile/ProfileEdit.tsx
+++ frontend/src/components/Profile/ProfileEdit.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import ProfileCompletion from "./ProfileCompletion";
-
+import { useStateContext } from "@/contexts/state-context";
+import type { User } from "@/types/user";
 
 type ProfileEditProps = {
+  user: User;
   isEdited: boolean;
   setIsEdited: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const ProfileEdit = ({ isEdited, setIsEdited }: ProfileEditProps) => {
+const ProfileEdit = ({ user, isEdited, setIsEdited }: ProfileEditProps) => {
+  const { state } = useStateContext();
+  const isOwner = state.user?.id === user.id;
 
   return (
     <>
-      {!isEdited && (
+      {isOwner && !isEdited && (
         <div className="flex h-full w-full items-center justify-center">
           <ProfileCompletion setIsEdited={setIsEdited} />
         </div>
